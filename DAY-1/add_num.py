83a71e30ce6fd8c5e7958064b0dde97163dbcf19
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2025-09-04 21:03:44 +0530

    Day-2

---
+++ DAY-1/add_num.py
@@ -0,0 +1,4 @@
+a = 10
+b = 21
+total = a + b
+print("The sum is:", total)
---
+++ DAY-2/data_type.py
@@ -0,0 +1,9 @@
+a = 10
+b = 21
+c = 5.5
+d = 'a'
+e = True
+print("Data type of a", type(a)) # Data type of a <class 'int'>
+print("Data type of c", type(c)) # Data type of c <class 'float'>
+print("Data type of d", type(d)) # Data type of d <class 'str'>
+print("Data type of e", type(e)) # Data type of e <class 'bool'>
