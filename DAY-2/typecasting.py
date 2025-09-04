46d49c3db26134e61212cf43e3e0dfa9bcef97f4
Bhavya Dhiman <bhavyadhiman2428@gmail.com>
2025-09-04 21:14:53 +0530

    Day-2

---
+++ DAY-2/typecasting.py
@@ -0,0 +1,4 @@
+a = int(input("Enter your first number:")) # typecasting string to integer
+b = int(input("Enter your second number:")) # typecasting string to integer
+total = a + b
+print("The sum is:", total)
